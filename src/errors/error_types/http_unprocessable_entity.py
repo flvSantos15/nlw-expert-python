ab07f9bdc62ee6ae9bab937160c4eb27874fee09
flvSantos15 <flvSantos300@gmail.com>
2024-02-13 21:54:15 -0300

    feat: add erro types and validators

---
+++ src/errors/error_types/http_unprocessable_entity.py
@@ -0,0 +1,9 @@
+class HttpUnprocessableEntityError(Exception):
+
+  def __init__(self, message: str ) -> None:
+    super().__init__(message)
+    self.message = message
+    self.name = "UnprocessableEntity"
+    self.status_code = 422
+
+# parei no 39:25
--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -4,12 +4,15 @@ from src.views.tag_creator_view import TagCreatorView
 
 from src.errors.error_handler import handle_errors
 
+from src.validator.tag_creator_validator import tag_creator_validator
+
 tags_routes_bp = Blueprint('tags_routes', __name__)
 
 @tags_routes_bp.route('/create_tag', methods=["POST"])
 def create_tags():
   response = None
   try:
+    tag_creator_validator(request)
     tag_creator_view = TagCreatorView()
 
     http_request = HttpRequest(body=request.json)
@@ -19,4 +22,4 @@ def create_tags():
 
   return jsonify(response.body), response.status_code
 
-# Parei no 12:25
+# parei no 35:55
